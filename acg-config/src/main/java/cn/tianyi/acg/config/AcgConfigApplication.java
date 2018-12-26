1220dec13d5089a21ac9256ec74a28818570e400
Tian <821793543@qq.com>
2018-12-26 16:23:04 +0800

    project init

--- acg-config/src/main/java/cn/tianyi/acg/config/AcgConfigApplication.java
+++ acg-config/src/main/java/cn/tianyi/acg/config/AcgConfigApplication.java
@@ -3,9 +3,13 @@ package cn.tianyi.acg.config;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@EnableConfigServer
+@EnableEurekaClient
 public class AcgConfigApplication {
 
 	public static void main(String[] args) {
